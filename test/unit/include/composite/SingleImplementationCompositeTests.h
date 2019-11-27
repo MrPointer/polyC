2e0f729147a3f0db51af82ba5f4b996b019acd7e
Timor Gruber <timor.gruber@gmail.com>
2019-11-28 00:14:47 +0200

    Added composite interface tests
    Composite interfaces are combined from actions & funcs.

---
+++ test/unit/include/composite/SingleImplementationCompositeTests.h
@@ -0,0 +1,40 @@
+//
+// Created by Timor Gruber on 11/27/19.
+// Copyright (c) 2019 Takpit. All rights reserved.
+//
+#ifndef POLYC_SINGLEIMPLEMENTATIONCOMPOSITETESTS_H
+#define POLYC_SINGLEIMPLEMENTATIONCOMPOSITETESTS_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <polyC/Implementation.h>
+#include "CompositeInterfaces.h"
+
+//region No Args
+
+DECLARE_IMPLEMENTATION(NoArgsCompositeInterfaceImpl, NoArgsCompositeInterface)
+
+void compositeActionNoArgsImpl();
+int compositeFuncNoArgsImpl();
+
+//endregion
+
+//region Single Arg
+
+DECLARE_IMPLEMENTATION(SingleArgCompositeInterfaceImpl, SingleArgCompositeInterface)
+
+void compositeActionSingleArgImpl(int);
+int compositeFuncSingleArgImpl(int);
+
+//endregion
+
+DECLARE_IMPLEMENTATION(NoArgsActionVariedCompositeInterfaceImpl, NoArgsActionVariedCompositeInterface)
+DECLARE_IMPLEMENTATION(NoArgsFuncVariedCompositeInterfaceImpl, NoArgsFuncVariedCompositeInterface)
+
+#ifdef __cplusplus
+}
+#endif
+#endif //POLYC_SINGLEIMPLEMENTATIONCOMPOSITETESTS_H
